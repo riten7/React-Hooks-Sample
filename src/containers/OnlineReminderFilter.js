d206261e858ee916433168259d4443655825df4a
rshakya <ritendra.shakya@activenetwork.com>
2020-03-17 16:40:57 +0545

    minor issue fixed

--- src/containers/OnlineReminderFilter.js
+++ src/containers/OnlineReminderFilter.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch} from 'react-redux';
 import { setFilter } from '../actions/actionCreators';
 import { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED} from '../actions/actionTypes';
 
-function OnlineReminderFilter(props) {
+function OnlineReminderFilter() {
 	const dispatch = useDispatch();
+	const filterReminders = useCallback((filterBy) => {
+		dispatch(setFilter(filterBy))
+	}, [dispatch]);
+
     return (
         <div className="reminder-filter">
 				<button type="button" className="btn btn-showAll"
-				  onClick={() => dispatch(setFilter(SHOW_ALL))}>All</button>
+				  onClick={() => filterReminders(SHOW_ALL)}>All</button>
 				<button type="button" className="btn btn-showActive"
-					onClick={() => dispatch(setFilter(SHOW_ACTIVE))}>Active</button>
+					onClick={() => filterReminders(SHOW_ACTIVE)}>Active</button>
 				<button type="button" className="btn btn-showCompleted"
-					onClick={() => dispatch(setFilter(SHOW_COMPLETED))}>Completed</button>
+					onClick={() => filterReminders(SHOW_COMPLETED)}>Completed</button>
 			</div>
     )
 }
